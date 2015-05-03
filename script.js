14fcbc381cbf902450fdd17caa807abf43fb7a18
mliq <liquori@gmail.com>
2015-05-03 11:59:10 -0500

    Holy crap I got the authorization to work finally

--- script.js
+++ script.js
@@ -1,23 +1,128 @@
 
+/*
+
+https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks
+  headers: {
+ 'Authorization' : 'Bearer BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L',
+ 'Accept' : 'application/json'
+ },
+ Client ID
+ e92000c6062c400c9b96d67d7df5e39e
+ Client Secret
+ 019eb23d5d0248e9bc92699f07e80c10
+
+*/
+
+//var access_token = 'BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L';
+
+//var redirectString = encodeURI('http://localhost:63342');
+var accessToken = null;
+
 $(document).ready(function(){
 
+    // Redirect for authorization.
+    if(window.location.hash == "") {
+        location = 'https://accounts.spotify.com/authorize/?client_id=e92000c6062c400c9b96d67d7df5e39e&response_type=token&redirect_uri=' + encodeURI('http://localhost:63342/weekend_assignment_W3/index.html') + '&scope=playlist-read-private';
+    } else {
+        // Comes back with: http://localhost:63342/weekend_assignment_W3/index.html#access_token=BQD2oCPPaVJGn00r2ydYwfX-hPliSZldYGhA48MstWzw6EDwLzLzbKi3-b286VzPlnglzYTaKkRc5sHQlrvj4i3m87iyiFob9AK15Wcf76Qg9NHqF81cr-kiArcGi3uNCWqUjEhOHC8-tgg3Y4U&token_type=Bearer&expires_in=3600
+
+        var hash = window.location.hash;
+        accessToken = hash.slice(14);
+        console.log(hash);
+
+        // #access_token=BQCv0tBWjrsOHmzsQAVXUOtwBfmuinP1ewwnoy65eiqI-YnD1syxz_3gF2drDlvgf4gnQBcJtK_44OzgkJFxprypaM7BEbGjJ6-FrH7_3Znfa72j-jHUGHo-ParaFZ3G4hC07nyodMX5w3NeC-4&token_type=Bearer&expires_in=3600
+        //
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            success: function (response) {
+                console.log(response);
+            }
+        });
+    }
+});
+
+/*
     $.ajax({
-        type: 'GET',
-        url: 'https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks',
-        crossDomain: true,
-        dataType: 'text',
-        success: function (response) {
+
+        url: 'https://accounts.spotify.com/authorize/?client_id=e92000c6062c400c9b96d67d7df5e39e&response_type=token&redirect_uri='+encodeURI('http://localhost:63342/weekend_assignment_W3/index.html')+'&scope=playlist-read-private',
+        //url: 'https://accounts.spotify.com/authorize',
+        //headers: {
+        //    'client_id': 'e92000c6062c400c9b96d67d7df5e39e',
+        //    'response_type': 'token'
+        //    'redirect_uri': redirectString
+            //'Access-Control-Allow-Origin': 'http://localhost:63342'
+        //},
+        success: function(response) {
             console.log(response);
         },
         error: function (xhr, status) {
             alert('Error: ' + status);
         }
     });
+*/
 
-    /*
-    URL: 'https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks'
-    Headers: headers: {
-        'Authorization' : 'Bearer BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L',
-            'Accept' : 'application/json'
-    },*/
+    //$.ajax({
+    //    url: 'https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks',
+    //    headers: {
+    //        'Authorization': 'Bearer ' + access_token,
+    //        'Access-Control-Allow-Origin': 'http://localhost:63342'
+    //    },
+    //    success: function(response) {
+    //        console.log(response);
+    //    },
+    //    error: function (xhr, status) {
+    //        alert('Error: ' + status);
+    //    }
+    //});
+    //$.ajax({
+    //    url: 'https://api.spotify.com/v1/me',
+    //    headers: {
+    //        'Authorization': 'Bearer ' + access_token
+    //    },
+    //    success: function(response) {
+    //        console.log(response);
+    //    },
+    //    error: function (xhr, status) {
+    //        alert('Error: ' + status);
+    //    }
+    //});
+
+
+    //$.ajax ({
+    //    type: 'GET',
+    //    dataType: 'json',
+    //    crossDomain: true,
+    //    dataType: 'text',
+    //    url: 'https://accounts.spotify.com/authorize/?client_id=e92000c6062c400c9b96d67d7df5e39e&response_type=code&redirect_uri='+encodeURI('http://localhost:63342/weekend_assignment_W3/index.html')+'&scope=playlist-read-private',
+    //    complete: function() {
+    //    console.log('ajax complete');
+    //    },
+    //    success: function(data) {
+    //    searchCallback(data.results);
+    //    }
+    //});
+
+//});
+/*
+
+var spotifyApi = new SpotifyWebApi();
+
+//spotifyApi.setAccessToken('BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L');
+
+$(document).ready(function(){
+
+    //spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+    //    if (err) console.error(err);
+    //    else console.log('Artist albums', data);
+    //});
+
+    spotifyApi.getPlaylist('1242734119', '0TfhLEsZWwxwmspQ9lQtaZ', function(err,data){
+        if (err) console.error(err);
+        else console.log('Artist albums', data);
+    });
+
 });
+*/
