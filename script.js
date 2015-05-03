ed713dc82d3056d2886e54ca45bc15da27e2903c
mliq <liquori@gmail.com>
2015-05-03 17:22:48 -0500

    Display of one user and picture working

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tra
 */
 
 var accessToken,playlistArr,userHref,trackName,trackArtist,trackHref,userName,userImg, userArr, i, personArr, j, trackArr, userId;
-var placeholder = "http://placekitten.com/200/300";
+var placeholder = "http://placekitten.com/g/200/300";
 
 Array.prototype.unique2 = function()
 { // Source: http://jszen.com/best-way-to-get-unique-values-of-an-array-in-javascript.7.html
@@ -28,13 +28,6 @@ Array.prototype.unique2 = function()
     }
     return r;
 };
-
-// Person constructor function
-//function Person(name){
-//    this.name = name;
-//    this.image = "";
-//    this.songs = [];
-//}
 
 function getStuff(obj) {
     // Array of tracks
@@ -55,10 +48,7 @@ function getStuff(obj) {
         }
     }
 
-    //for (i=0; i < personArr.length; i++) {
-    //    getUserInfo(personArr[i]);
-    //    console.log("if");
-    //}
+    displayStuff(personArr['5nahalf']);
 }
 
 function getUserInfo(person){
@@ -86,64 +76,9 @@ function getUserInfo(person){
         }
     });
 
-};
-
-/* V1
-    // Cycle through to collect all unique users.
-    // First get all user hrefs, then reduce to unique.
-    for(i = 0; i<playlistArr.length; i++){
-        userArr.push(playlistArr[i].added_by.href);
-    }
-    userArr = userArr.unique2();
-
-    // Now make all of those objects with href, name, image, and songs array
-
-    for(i = 0; i < userArr.length; i++){
-        // Get user's profile from href
-        userHref = userArr[i];
-        $.ajax({
-            url: userHref,
-            headers: {
-                'Authorization': 'Bearer ' + accessToken
-            },
-            success: function (response) {
-
-                // Conditional for no name
-                if( response.display_name !== null) {
-                    userName = response.display_name;
-                } else {
-                    userName = response.id;
-                }
-                // Conditional for no image
-                if (response.images.length != 0){
-                    userImg = response.images[0].url;
-                } else {
-                    userImg = placeholder;
-                }
-                personArr.push(new Person(userHref, userName, userImg));
-            }
-        });
-    }
-
-    // Wait for all above ajax requests to complete
-    function checkIfFinished(){
-        //console.log(personArr.length == userArr.length);
-        return(personArr.length == userArr.length);
-    }
-
-    var checkInterval = setInterval(function(){
-        if(checkIfFinished()){
-            clearInterval(checkInterval);
-            songCheck();
-        }
-    }, 100);
-
 }
 
-function songCheck(){
-    // Add songs to person objects.
-    // Loop through each song, subloop through each object.
-
+/*
     // Extract key pieces of data
     userHref = playlistArr[0].added_by.href;
     trackArr = playlistArr[0].track;
@@ -161,10 +96,43 @@ function songCheck(){
 
 */
 
-function displayStuff(){
+function displayStuff(user){
+    console.log(user);
+
+    // Wait for user data to be retrieved, checking every 100ms.
+    function checkIfFinished(){
+        console.log(typeof user['userName']);
+        return(typeof user['userName']== 'string');
+    }
+    var checkInterval = setInterval(function(){
+        if(checkIfFinished()){
+            clearInterval(checkInterval);
+
+            // Do the displaying:
+            $('.results').append('<div class="gallery-head">' + user['userName'] + '</div>');
+            $('.results').append('<div class="gallery-img"><img src="' + user['userImg'] + '"></div>');
+
+        }
+    }, 100);
+
+    // Seems like problem might be that the data is not here yet.
+
+
     // Display
-    $('.results').append('Added By: ' + userName + '<img src=' + userImg + '>' + '<br>Song: ' + trackName + '<br>Artist: ' + trackArtist + '<br><a href=' + trackHref + '>Play song</a>');
+    //$('.results').append('Added By: ' + userName + '<img src=' + userImg + '>' + '<br>Song: ' + trackName + '<br>Artist: ' + trackArtist + '<br><a href=' + trackHref + '>Play song</a>');
+
+    //$gallery = $('.results');
+    // Build the new game's information
+    //$gallery.empty();
+    //$('.results').append('<div class="gallery-head">' + user['userName'] + '</div>');
+    //$gallery.append('<div class="gallery-img"><img src="' + user.userImg + '"></div>');
+    //$gallery.append('<div class="gallery-platforms"><h4>Platforms</h4><p>' + user.platforms + '</p></div>');
+    //$gallery.append('<div class="gallery-desc"><h4>Description</h4><p>' + this.deck + '</p><button class="btn btn-default js-btn-read">Show Details</button></div>');
+    //$gallery.append('<div class="gallery-details hidden">' + this.description + "</div>");
+    // Fade in the new game
+    //$gallery.fadeIn();
 }
+
 
 $(document).ready(function(){
 
@@ -189,68 +157,3 @@ $(document).ready(function(){
         });
     }
 });
-
-/*
-
-    //$.ajax({
-    //    url: 'https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks',
-    //    headers: {
-    //        'Authorization': 'Bearer ' + access_token,
-    //        'Access-Control-Allow-Origin': 'http://localhost:63342'
-    //    },
-    //    success: function(response) {
-    //        console.log(response);
-    //    },
-    //    error: function (xhr, status) {
-    //        alert('Error: ' + status);
-    //    }
-    //});
-    //$.ajax({
-    //    url: 'https://api.spotify.com/v1/me',
-    //    headers: {
-    //        'Authorization': 'Bearer ' + access_token
-    //    },
-    //    success: function(response) {
-    //        console.log(response);
-    //    },
-    //    error: function (xhr, status) {
-    //        alert('Error: ' + status);
-    //    }
-    //});
-
-
-    //$.ajax ({
-    //    type: 'GET',
-    //    dataType: 'json',
-    //    crossDomain: true,
-    //    dataType: 'text',
-    //    url: 'https://accounts.spotify.com/authorize/?client_id=e92000c6062c400c9b96d67d7df5e39e&response_type=code&redirect_uri='+encodeURI('http://localhost:63342/weekend_assignment_W3/index.html')+'&scope=playlist-read-private',
-    //    complete: function() {
-    //    console.log('ajax complete');
-    //    },
-    //    success: function(data) {
-    //    searchCallback(data.results);
-    //    }
-    //});
-
-//});
-/*
-
-var spotifyApi = new SpotifyWebApi();
-
-//spotifyApi.setAccessToken('BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L');
-
-$(document).ready(function(){
-
-    //spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
-    //    if (err) console.error(err);
-    //    else console.log('Artist albums', data);
-    //});
-
-    spotifyApi.getPlaylist('1242734119', '0TfhLEsZWwxwmspQ9lQtaZ', function(err,data){
-        if (err) console.error(err);
-        else console.log('Artist albums', data);
-    });
-
-});
-*/
