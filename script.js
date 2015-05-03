084bb7050e1d5668a8af27dc41185361cee739fd
mliq <liquori@gmail.com>
2015-05-03 10:55:03 -0500

    CORS not working either, moving on to library

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 
 $(document).ready(function(){
 
-
-    $.getJSON('https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks', )
-
     $.ajax({
         type: 'GET',
         url: 'https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks',
-        headers: {
-            'Authorization' : 'Bearer BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L',
-            'Accept' : 'application/json'
-        },
-        //beforeSend: function(xhr) {
-        //    xhr.setRequestHeader('Authorization', 'Bearer BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L');
-        //},
-        //processData: false,
         crossDomain: true,
-        dataType: 'json',
-        //accepts: 'application/json',
-        contentType: 'application/json',
-        success: function (data) {
-            console.log(data);
+        dataType: 'text',
+        success: function (response) {
+            console.log(response);
         },
-        error: function (xhr, status, error) {
-            alert('Error: ' + status + " " + error);
-
+        error: function (xhr, status) {
+            alert('Error: ' + status);
         }
     });
 
-    $.get('https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks'),
+    /*
+    URL: 'https://api.spotify.com/v1/users/1242734119/playlists/0TfhLEsZWwxwmspQ9lQtaZ/tracks'
+    Headers: headers: {
+        'Authorization' : 'Bearer BQBSWPXdOapkLMqzKjvIYmIItqGtK2tUZhlFmeazjyg3I0rqH7YE_p4rXZgJ8jGNfgBggYy5tue1L',
+            'Accept' : 'application/json'
+    },*/
 });
